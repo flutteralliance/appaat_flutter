14764b030902b93e0c430732ca54cda3870718ed
“pengMaster”  <946549990@qq.com>
2019-08-27 15:33:18 +0800

    add_login

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -137,7 +137,7 @@ class LoginPageState extends BaseState<LoginPage> {
         Flexible(child: pwdInputView()),
         Padding(
           padding: EdgeInsets.only(top: h(120)),
-          child: GradientRedButton("登  录", width: 670, height: 88，onTap:onClickLogin),
+          child: GradientRedButton("登  录", width: 670, height: 88,onTap:onClickLogin),
         )
       ],
     );
